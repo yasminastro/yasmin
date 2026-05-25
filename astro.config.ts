71e818e46fd336fa071900757c769217f136e535
yasminastro <yasminastro@outlook.com>
2026-05-25 08:36:09 +0200

    fix site and base path

--- astro.config.ts
+++ astro.config.ts
@@ -18,7 +18,7 @@ import { transformerFileName } from "./src/utils/transformers/fileName";
 import config from "./astro-paper.config";
 
 export default defineConfig({
-  site: config.site.url,
+  site: "https://yasminastro.github.io",
   base: "/yasmin",
   integrations: [
     mdx(),
